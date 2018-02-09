3be0962199e331cdb1620ca77c7c09c2926fb448
Jed <jed-frey@users.noreply.github.com>
2018-02-09 13:11:29 -0500

    Added comments, added requirements. updated gitignore.

--- make_all.sh
+++ make_all.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
+# Generate g-code for a full permutation of profile settings.
+
+# List all of the print profiles.
 for PRINT_ in $(ls slic3r_profiles/print/*)
 do
 
+# List all of the printer profiles.
 for PRINTER_ in $(ls slic3r_profiles/printer/*)
 do
 
+# List all of the filament profiles
 for FILAMENT_ in $(ls slic3r_profiles/filament/*)
 do
 export PRINT=$(basename $PRINT_)
 export PRINTER=$(basename $PRINTER_)
 export FILAMENT=$(basename $FILAMENT_)
 
+# Run make with the given profiles.
+#TODO: benchmark and see which way is faster.
 THREADS=1 make -j8
 
 done
